74df9d88db4223e363a356365e133149080caf8a
GigglesBrook <chris@stanforth.net>
2016-08-03 13:42:45 +0100

    java controlled calendar table
    finally I have understood a little about overriding inner html

--- AppointmentDemo/www/scripts/index.js
+++ AppointmentDemo/www/scripts/index.js
@@ -67,31 +67,20 @@ function processAppointmentResults(transaction, resultAppointments)
 
 function GenerateSingleMonthCalender()
 {
-    document.getElementById("dayValue").innerHTML = '17';
+    document.getElementById("dayValue").innerHTML = '19';
     //    <a href="">Â«</a><span>17</span><a href="#">Â»</a>   // day should include this but it doesn't work as I'd like
 
     document.getElementById("monthValue").innerHTML = 'Febuary';
 
+    document.getElementById("calendarDaysBody").innerHTML = '<tr><td class="inactive">30</td><td class="inactive">31</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>';
 
+    document.getElementById("calendarDaysBody").innerHTML += '<tr><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td><td>12</td></tr>';
 
+    document.getElementById("calendarDaysBody").innerHTML += '<tr><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td></tr>';
 
-//    document.getElementById("red-calendarWrapper").innerHTML = '<div id="red-calendarWrapper">';
+    document.getElementById("calendarDaysBody").innerHTML += '<tr><td>20</td><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td><td>26</td></tr>';
 
-//    document.getElementById("red-calendarWrapper").innerHTML += '<div id="tableHeader">';
-
-//    document.getElementById("red-calendarWrapper").innerHTML += '<div class="day">';
-
-//    document.getElementById("red-calendarWrapper").innerHTML += '<a href="">Â«</a><span>14</span><a href="#">Â»</a>';
-
-//    document.getElementById("red-calendarWrapper").innerHTML += '</div>';
-           
-//    document.getElementById("red-calendarWrapper").innerHTML += '<div class="month">';
-               
-//    document.getElementById("red-calendarWrapper").innerHTML += 'FebuaryTest';
-           
-//    document.getElementById("red-calendarWrapper").innerHTML += '</div>';
-
-//    document.getElementById("red-calendarWrapper").innerHTML += '</div>';
+    document.getElementById("calendarDaysBody").innerHTML += '<tr><td>27</td><td>28</td><td>29</td><td>30</td><td>31</td><td class="inactive">01</td><td class="inactive">02</td></tr>';
 }
 
 
